da4360699b347110562150ef10cf02da3ecfb08c
brennanferguson14-create <brennanferguson14@gmail.com>
2026-08-28 17:42:25 -0400

    Add files via upload

--- recycle.py
+++ recycle.py
@@ -15,7 +15,7 @@ FINAL_LEVEL = 5
 
 START_SPEED = 5
 
-ITEMS = ["chipsimg","bagimg","trashbagimg","bottleimg"]
+ITEMS = ["chips","bag","trashbag","bottle","battery"]
 
 gameover = False
 gamecomplete = False
@@ -24,7 +24,7 @@ Items = []
 animations = []
 
 def display_message(title,subtitle):
-    screen.draw.text(title,fontsize=50, center=CENTRE.color="white")
+    screen.draw.text(title,fontsize=50, center=CENTRE,color="white")
     screen.draw.text(subtitle,fontsize=30, center = (CENTER.X,CENTRE.Y+30),color="white")
 
 def draw():
@@ -49,8 +49,67 @@ def makeitems(level):
 
 def update():
     global Items
-    if len(Items)==0
-    Items= makeitems(currentlevel)
+    if len(Items)==0:
+        Items= makeitems(currentlevel)
 
+
+def getoptiontocreate(level):
+    itemstocreate=["bag"]
+    for i in range(0,level):
+        randomoption = random.choice(ITEMS)
+        itemstocreate.append(randomoption)
+    return itemstocreate
+
+def createitems(itemstocreate):
+    newitems=[]
+    for i in itemstocreate:
+        item = Actor(i + "img")
+        newitems.append(item)
+    return newitems
+
+def layoutitems(itemstolayout):
+    numberofgaps = len(itemstolayout)
+    gapsize = WIDTH/numberofgaps
+    random.shuffle(itemstolayout)
+    for index,item in enumerate (itemstolayout):
+        newxpos = (index+1)*gapsize
+        item.x = newxpos
+
+
+def handlegameover():
+    global gameover
+    gameover = True
+
+def onmousedown(pos):
+    global items, currentlevel
+    for i in Items:
+        if i.collidepoint(pos):
+            if "paper" in i.image:
+                handlegamecomplete()
+            else:
+                handlegameover()
+
+def handlegamecomplete():
+    global currentlevel, Items, animations, gamecomplete
+    stopanimations(animations)
+    if currentlevel == FINAL_LEVEL:
+        gamecomplete = True
+    else:
+        currentlevel += 1
+        Items = []
+        animations = []
+
+def animateitems(itemstoanimate):
+    global animations
+    for i in itemstoanimate:
+        duration = START_SPEED - currentlevel
+        i.anchor = {"center","bottom"}
+        animation = animate(i, duration=duration, onfinished=handlegameover, y=HEIGHT)
+        animations.append(animation)
+
+def stopanimations(animationstostop):
+        for animation in animationstostop:
+            if animation.running:
+                animation.stop
 
 pgzrun.go()
